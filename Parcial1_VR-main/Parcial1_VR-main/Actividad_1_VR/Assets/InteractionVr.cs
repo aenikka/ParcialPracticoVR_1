d44c9a34916c0e5ce4b5799bbd2f20b5c78521b6
Pietrovp1001 <pietro.villano@upb.edu.co>
2023-03-01 00:59:47 -0500

    Interactions Script

---
+++ Parcial1_VR-main/Parcial1_VR-main/Actividad_1_VR/Assets/InteractionVr.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tilia.Interactions.Controllables;
+using Tilia.Interactions.Controllables.AngularDriver;
+using Unity.VisualScripting;
+
+public class InteractionVr : MonoBehaviour
+{
+   [SerializeField] AngularDriveFacade angularDrive;
+   [SerializeField] float targetValue;
+   [SerializeField] ParticleSystem particleSystem;
+   public bool isInteracting;
+   private void Update()
+   {
+      if (angularDrive.TargetValue >= targetValue && !isInteracting)
+      {
+         isInteracting = true;
+         ActiveParticleSystemInteraction();
+         Debug.Log("soy una interaccion");
+      }
+   }
+   
+   void ActiveParticleSystemInteraction()
+   {
+      particleSystem.Play();
+   }
+}
